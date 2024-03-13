3420142bca0e586e0502968d37840dba22b01901
Pranav <srinivaspranav1031@gmail.com>
2024-03-13 23:08:59 +0530

    "."

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,9 @@ const SliderContainer = () => {
     return (
       <div className="sliderContainers">
         <img src={Earth} alt="Earth" className='sliderImage' />
+        {/* <video autoPlay loop muted id='video' className='sliderImage'>
+          <source src={Earth} type='video/mp4'/>
+          </video> */}
       </div>
     );
   };
@@ -28,9 +31,14 @@ const SliderContainer = () => {
       <div className="eventsSection">
         <div className="eventCard">
           <div className="eventCardContent">
+            <p className='eventtype'>#spotlight</p>
             <h3 className="eventTitle">Mobile App Development By Flutter </h3>
-            <p className="eventDescription">Event description goes here.</p>
-            <button>Read More  </button>
+            <p className="eventDescription">Competitive coding refers to the practice of solving algorithmic and coding problems within a specified time limit, often in a competitive environment such as coding contests or online platforms. The primary goal is to write efficient and correct code to solve a given problem statement.</p>
+            <button className='eventbutton'>Read More
+    <div class="arrow-wrapper">
+        <div class="arrow"></div>
+        </div>
+        </button>
           </div>
           <img className="eventImage" src={Mobile} alt="Event Image" />
         </div>
